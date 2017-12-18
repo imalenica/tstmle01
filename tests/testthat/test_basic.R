1631c13d693ad1b9afa2c4782dca9db04b394de4
podTockom <imalenica.stat@gmail.com>
2017-12-18 12:16:40 -0800

    update smaller dataset and example

--- tests/testthat/test_basic.R
+++ tests/testthat/test_basic.R
@@ -1,16 +1,12 @@
 library(Hmisc)
-library(doParallel)
+library(doParallel)  
 library(parallel)
 library(foreach)
+library(tstmle01)
 
 set.seed(2)
-data("ts_samp_data")
-data<-ts_samp_data
-names<-row.names(data)
-
-# Make it very easier:
-data <- data.frame(data = data[1:303, ])
-row.names(data) <- names[1:303]
+data("ts_samp_data_ex")
+data=ts_samp_data_ex
 
 # Get initial estimates of the process:
 fit <- initEst(data, t=5, freqW = 2, freqA = 2, freqY = 2)
